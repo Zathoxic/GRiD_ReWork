42b960a374f274aad0f2f9a12bdc4b416d9cc76a
Lukas Klein Haneveld <sakulkh@gmail.com>
2016-02-18 09:59:18 +0100

    no message

--- inc/header.php
+++ inc/header.php
@@ -11,29 +11,19 @@
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-<<<<<<< HEAD
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="index.php">GRID IMO</a>
-=======
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../tpl/index.tpl">GRID IMO</a>
->>>>>>> 6928f0e98f097875b66728e7547b5c6976b1558c
+                <a class="navbar-brand" href="index.php">GRID IMO</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+
                     <li><a href="../contact.php">Contact</a></li>
                     <li><a href="../admin.php">Admin</a></li>
                     <li><a href="#">Link</a></li>
